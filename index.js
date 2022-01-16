f9244b0e8d6d602f41ef51d6c00dd0084fa19170
Paulo Eduardo de Sordi Gomes <pauloeduardodesordigomes@gmail.com>
2022-01-15 21:51:22 -0300

    Fix lint

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
-// app.get('/', (_request, response) => {
-//   response.status(HTTP_OK_STATUS).send();
-// });
+app.get('/', (_request, response) => {
+  response.status(HTTP_OK_STATUS).send();
+});
 
 app.use('/talker', talkRouter);
 
